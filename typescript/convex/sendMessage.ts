0678bf50976f3fce3476a73858946f7bb933d48e
Jamie Turner <jamwt@convex.dev>
2023-04-03 15:50:15 -0400

    Object passing from Python to typescript.

--- typescript/convex/sendMessage.ts
+++ typescript/convex/sendMessage.ts
@@ -1,7 +1,12 @@
 import { mutation } from "./_generated/server";
 
+type Message = {
+  body: string;
+  author: string;
+};
+
 export default mutation(
-  async ({ db }, { body, author }: { body: string; author: string }) => {
+  async ({ db }, { body, author }: Message) => {
     const message = { body, author };
     await db.insert("messages", message);
   }
---
+++ typescript/test.py
@@ -0,0 +1,6 @@
+from convex import ConvexClient
+from pprint import pprint
+client = ConvexClient('https://dapper-quail-967.convex.cloud')
+messages = client.query("listMessages")
+pprint(messages)
+client.mutation("sendMessage", {'author': 'Jamie', 'body': 'This is good'})
